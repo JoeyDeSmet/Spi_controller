abdc337fcc75e72bf5f4d4014aa1088910b341fa
madness007 <arno.schoutteten@gmail.com>
2022-04-20 15:17:57 +0200

    color max and min values

--- src/Color.ts
+++ src/Color.ts
@@ -4,9 +4,13 @@ export class Color {
   public blue:  number;
 
   constructor(red: number, green: number, blue: number) {
-    this.red = red;
-    this.green = green;
-    this.blue = blue;
+    this.red = (red <= 0) ? 1 : red;
+    this.green = (green <= 0) ? 1 : green;
+    this.blue = (blue <= 0) ? 1 : blue;
+
+    this.red = (this.red >= 255) ? 255 : this.red;
+    this.green = (this.green >= 255) ? 255 : this.green;
+    this.blue = (this.blue >= 255) ? 255 : this.blue;
   }
 }
 
